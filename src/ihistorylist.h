96b23b15e33b19aca4bdf4f059dd2a77178ede1d
Farley Runkel <farleyrunkel@gmail.com>
2024-05-07 22:24:14 +0800

    fix ...

--- src/ihistorylist.h
+++ src/ihistorylist.h
@@ -17,7 +17,7 @@ public:
     QSize sizeHint(const QStyleOptionViewItem& option, const QModelIndex& index) const override
     {
         QSize size = QStyledItemDelegate::sizeHint(option, index);
-        size.setHeight(32); // 设置item的高度为50像素
+        size.setHeight(32);
         return size;
     }
 
@@ -53,50 +53,67 @@ protected:
         setUniformItemSizes(true);
     }
     void mouseMoveEvent(QMouseEvent* event) override {
-
+        QListWidget::mouseMoveEvent(event);
         auto index = this->indexAt(event->pos());
 
         if (index.isValid()) {
             auto item_rect = this->visualRect(index);
 
-            // 设置按钮的位置
-            widget->setGeometry(item_rect);
-            widget->setVisible(true);
+            for (int i = 1 ; i<= m_buttons.size(); i++) {
+                setSubGeometry(item_rect, i);
+                getSubButton(i)->setVisible(true);
+            }
+        }
+        else {
+            for (int i = 1 ; i<= m_buttons.size(); i++) {
+                getSubButton(i)->setVisible(false);
+            }
         }
     }
 private:
     void initButtonsWidget() {
-        widget = new QWidget(this);
-        QHBoxLayout *layout = new QHBoxLayout(widget);
 
-        QPushButton *moreButton = createButton(":/icon/more-horiz.svg", "More");
-        QPushButton *archButton = createButton(":/icon/archive-book.svg", "Archive");
+        addSubButton(":/icon/archive-book.svg", "Archive");
+        addSubButton(":/icon/more-horiz.svg", "More");
 
+        auto moreButton = getSubButton(2);
         QMenu *menu = new QMenu(moreButton);
         menu->addAction("Share");
         menu->addAction("Rename");
         menu->addAction("Delete chat");
         moreButton->setMenu(menu);
-
-        layout->addItem(new QSpacerItem(0, 0, QSizePolicy::Expanding, QSizePolicy::Expanding));
-        layout->addWidget(moreButton);
-        layout->addWidget(archButton);
-
-        widget->setLayout(layout);
-        widget->setObjectName("buttonsWidget");
-        widget->setStyleSheet("background: rgba(255, 255, 255, 128);");
-        widget->setVisible(false);
     }
 
-    QPushButton *createButton(const QString &iconPath, const QString &tooltip) {
-        QPushButton *button = new QPushButton(QIcon(iconPath), "");
+    void setSubGeometry(const QRect& rect, int i) {
+        if (i <= 0) return ;
+        int margin = rect.height() * 0.25;
+        int h = rect.height() - (margin * 2);
+        int w = h;
+        int y = rect.y() + margin;
+        int x = rect.x() + rect.width() - i * ( w  + margin);
+
+        getSubButton(i)->setGeometry(x, y, w, h);
+    }
+
+    QPushButton* getSubButton(int i) {
+        if (i <= 0) return nullptr;
+        return m_buttons[i-1];
+    }
+
+    void addSubButton(const QString& icon, const QString &tooltip = "") {
+        auto button = new QPushButton(QIcon(icon), "", this);
+        button->setFlat(true);
+        button->setFocusPolicy(Qt::NoFocus);
         button->setFixedSize(QSize(16, 16));
         button->setToolTip(tooltip);
-        button->setFlat(true);
-        return button;
+        button->setVisible(false);
+
+        m_buttons.push_back(button);
     }
+
 private:
-    QWidget *widget;
+    std::vector<QPushButton*> m_buttons;
+
 };
 
 #endif // IHISTORYLIST_H
