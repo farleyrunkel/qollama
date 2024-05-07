3385467eaef6f91df1c9534476815fdf316f0541
Farley Runkel <farleyrunkel@gmail.com>
2024-05-07 16:39:14 +0800

    add mainwindow h

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,4 +1,4 @@
-#include "mainwindow.h"
+#include "src/mainwindow.h"
 #include "./ui_mainwindow.h"
 #include <QVBoxLayout>
 #include "messagebox.h"
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include "../mainwindow.h"
+#include "mainwindow.h"
 
 #include <QApplication>
 #include <QFile>
