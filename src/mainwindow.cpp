6eb6e887195d15c98470e8265510f41772c8cac8
Farley Runkel <farleyrunkel@gmail.com>
2024-05-08 11:54:54 +0800

    ref

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,9 +1,9 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QVBoxLayout>
-#include "messagebox.h"
 #include <QDialog>
 #include "ioverlaybutton.h"
+#include <QLabel>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -29,22 +29,27 @@ void MainWindow::on_inputLine_returnPressed()
 {
     QString text = ui->inputLine->text();
 
-    if (text.isEmpty()) {
+    if (text.trimmed().isEmpty()) {
         return;
     }
 
-    QString user = "John Doe";
-    QIcon icon = QIcon(":/icon/uranus.svg");
+    auto userItem = new QListWidgetItem(this->ui->userButton->icon(), this->ui->userButton->text(), ui->chatList);
+    userItem->setFlags(userItem->flags() | Qt::ItemIsSelectable | Qt::ItemIsEnabled);
 
-    auto message = new MessageBox(user, icon, text , ui->chatList);
-    auto item = new QListWidgetItem(ui->chatList);
+    ui->chatList->addItem(userItem);
 
+    auto messageItem = new QListWidgetItem(ui->chatList);
+    auto message = new QLabel(text , ui->chatList);
+    message->setMargin(25);
+    message->setWordWrap(true);
+    message->setAlignment(Qt::AlignTop);
     auto size = message->sizeHint();
-    item->setSizeHint(size);
-    item->setFlags(item->flags() | Qt::ItemIsSelectable | Qt::ItemIsEnabled);
+    messageItem->setSizeHint(size);
+    messageItem->setFlags(messageItem->flags() | Qt::ItemIsSelectable | Qt::ItemIsEnabled);
 
-    ui->chatList->addItem(item);
-    ui->chatList->setItemWidget(item, message);
+    ui->chatList->addItem(messageItem);
+    ui->chatList->setItemWidget(messageItem, message);
+
     ui->chatList->update();
     ui->inputLine->clear();
 }
