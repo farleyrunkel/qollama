c5e425a69b5093d5bfe3f7ec880f1142262ecb69
Farley Runkel <farleyrunkel@gmail.com>
2024-05-09 16:13:44 +0800

    fix chat text unselectable

--- src/imessagebox.cpp
+++ src/imessagebox.cpp
@@ -7,8 +7,12 @@ IMessagebox::IMessagebox(QWidget *parent)
 {
     ui->setupUi(this);
 
+    ui->iconLabel->setTextInteractionFlags(Qt::TextSelectableByMouse | Qt::TextSelectableByKeyboard);
+    ui->userLabel->setTextInteractionFlags(Qt::TextSelectableByMouse | Qt::TextSelectableByKeyboard);
+
     ui->chatLabel->setWordWrap(true);
     ui->chatLabel->setAlignment(Qt::AlignTop);
+    ui->chatLabel->setTextInteractionFlags(Qt::TextSelectableByMouse | Qt::TextSelectableByKeyboard);
 };
 
 
