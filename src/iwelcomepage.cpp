2d129cb5612abd7f23522a6be626ef10752bb04e
Farley Runkel <farleyrunkel@gmail.com>
2024-05-15 13:41:25 +0800

    fix

--- src/iwelcomepage.cpp
+++ src/iwelcomepage.cpp
@@ -6,8 +6,8 @@ IWelcomePage::IWelcomePage(QWidget *parent)
     , ui(new Ui::IWelcomePage)
 {
     ui->setupUi(this);
-    ui->welcomeLabel1->setText("Tell me a random fun fact about the Roman Empire");
-    ui->welcomeLabel2->setText("Create a personal webpage for me, all in a single file. Ask me 3 questions first on whatever you need to know.");
-    ui->welcomeLabel3->setText("Write a short-and-sweet text message inviting my neighbor to a barbecue.");
-    ui->welcomeLabel4->setText("Explain superconductors like I'm five years old.");
+    ui->welcomeText1->setText("Tell me a random fun fact about the Roman Empire");
+    ui->welcomeText3->setText("Create a personal webpage for me, all in a single file. Ask me 3 questions first on whatever you need to know.");
+    ui->welcomeText2->setText("Write a short-and-sweet text message inviting my neighbor to a barbecue.");
+    ui->welcomeText4->setText("Explain superconductors like I'm five years old.");
 }
--- src/iwelcomepage.h
+++ src/iwelcomepage.h
@@ -22,12 +22,31 @@ private slots:
 protected:
     void mousePressEvent(QMouseEvent *event) override {
         if (event->button() == Qt::LeftButton) {
+            qDebug() << "Left button pressed";
 
-            auto child = static_cast<QLabel* >( this->childAt(event->pos()));
-            emit send(child->text());
+            auto child = this->childAt(event->pos());
+
+            if (child) {
+                qDebug() << "Child widget found at" << event->pos();
+                qDebug() << "Looking at QFrame with name:" << child->objectName();
+
+                QString name = QString("welcomeText") + child->objectName().back();
+                qDebug() << "Looking for QLabel with name:" << name;
+
+                QLabel* label = this->findChild<QLabel*>(name);
+                if (label) {
+                    qDebug() << "Found QLabel with text:" << label->text();
+                    emit send(label->text());
+                } else {
+                    qDebug() << "No matching QLabel found";
+                }
+            } else {
+                qDebug() << "No child widget found at" << event->pos();
+            }
         }
         QWidget::mousePressEvent(event);
     }
+
 
 private:
     Ui::IWelcomePage *ui;
