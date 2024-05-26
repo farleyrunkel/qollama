7e85a887482396400d4ecc40ff5fbb7a94972659
Farley Runkel <farleyrunkel@gmail.com>
2024-05-26 18:04:49 +0800

    adjust struction

---
+++ external/ollama/ollama.hpp
@@ -0,0 +1,11 @@
+#ifndef OLLAMA_H
+#define OLLAMA_H
+
+#include <asio/asio.hpp>
+#include <nlohmann/json.hpp>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <optional>
+#include <future>
